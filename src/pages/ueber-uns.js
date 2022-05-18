8597584e68de0fef8f9572225e751ed2c3cfaf4d
janfruehtrunk <52034243+janfruehtrunk@users.noreply.github.com>
2022-05-18 13:19:52 +0200

    Update ueber-uns.js

--- src/pages/ueber-uns.js
+++ src/pages/ueber-uns.js
@@ -13,9 +13,8 @@ const SecondPage = () => (
       <Text as="p">Frühtrunk-Dach ist Ihr Partner, wenn es um das Dach geht.</Text>
       Dachdeckermeister und Spengler Jan Frühtrunk zeichnet sich mit
       über 15 Jahren Berufserfahrung in England, Deutschland und Österreich
-      aus. Ob Einfamilienhaus, Villa, Zinshaus oder Kirche - Frühtrunk Dach bringt die nötige Erfahrung mit
-     .
-    </Text>
+      aus. Ob Einfamilienhaus, Villa, Zinshaus oder Kirche - Frühtrunk Dach bringt die nötige Erfahrung mit.
+      </Text>
     <Text as="h3" fontSize={1}></Text>
     <Text as="p">Jan Frühtrunk</Text>
     <StaticQuery
