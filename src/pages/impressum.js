27e207079a70909a2448efffd8b58e5dac6329e9
janfruehtrunk <52034243+janfruehtrunk@users.noreply.github.com>
2022-09-16 09:22:52 +0200

    Update impressum.js

--- src/pages/impressum.js
+++ src/pages/impressum.js
@@ -10,8 +10,8 @@ const SecondPage = () => (
     <Text as="p">
       <strong>Frühtrunk Dach</strong><br />
       Jan Frühtrunk<br />
-      Wiedner Hauptstraße 65<br />
-      1040 Wien<br />
+      Franzensgasse 11/1<br />
+      1050 Wien<br />
       UID: ATU77757303<br /><br />
       Mitglied der WKO Landesinnung Dachdecker, Glaser und Spengler<br />
       Gerichtsstand Wien<br />
